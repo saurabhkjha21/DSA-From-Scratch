a620e91a6cbd75a57697dc418af1856d360b7a3e
saurabhkjha21 <saurabhkjha21@gmail.com>
2026-02-15 17:24:11 +0530

    BFS Tree - in this we explore the node in level-order means instead of going depth we explore the neighbours node

---
+++ src/main/java/Tree/TreeBFS.java
@@ -0,0 +1,45 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeBFS {
+    static class Node{
+        int data;
+        Node left, right;
+
+        Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
+    public static void levelOrder(Node root){
+        if(root==null) return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            Node current = queue.poll();
+            System.out.print(current.data+" ");
+
+            if(current.left!=null){
+                queue.add(current.left);
+            }
+            if(current.right!=null){
+                queue.add(current.right);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(0);
+        root.left = new Node(1);
+        root.right = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+        levelOrder(root);
+
+    }
+}
