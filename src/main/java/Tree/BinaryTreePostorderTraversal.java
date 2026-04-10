0294512d327009ec0319eee56290d51693a51294
saurabhkjha21 <saurabhkjha21@gmail.com>
2026-04-10 13:27:33 +0530

    Create BinaryTreePostorderTraversal.java

---
+++ src/main/java/Tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,33 @@
+//Binary Tree Postorder Traversal - LeetCode 145
+//Time Complexity = O(n)
+//Space Complexity = O(n)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorder(root, list);
+        return list;
+    }
+    public static void postorder(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+}
